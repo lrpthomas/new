8020c7e9ff059f2df36f508f17eb888db551a696
CRTA <124310581+lrpthomas@users.noreply.github.com>
2025-06-21 05:19:57 -0700

    [MP-6] add DATA_STORE env option

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import memoryStore from './memory-store';
+import fileStore from './file-store';
 export type { DataStore } from './data-store';
 
-// In the future this could switch based on config
-export const dataStore = memoryStore;
+// Select data store based on environment
+export const dataStore =
+  process.env.DATA_STORE === 'file' ? fileStore : memoryStore;
 
 export default dataStore;
